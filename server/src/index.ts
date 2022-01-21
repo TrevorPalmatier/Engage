ad3ea2ea007cc06fd6f39bfe6d06e3739d7aaad6
Kaley Wilson <kaley.wilson@hope.edu>
2022-01-21 14:12:08 -0500

    push index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,20 @@
 import "reflect-metadata";
 import bodyParser from "body-parser";
-import { createConnection } from "typeorm";
 import { login, signup, isAuth } from "./auth/auth";
+import { User } from "./entity/User";
 import express from "express";
 import cors from "cors";
 import { Request, Response } from "express";
 import { Routes } from "./routes";
+import { getConnectionOptions, ConnectionOptions, createConnection} from "typeorm";
+import dotenv from 'dotenv';
+dotenv.config();
 
 createConnection()
 	.then(async (connection) => {
 		// create express app
 		const app = express();
-		const port = 3000; // default port to listen
+		//const port = 3000; // default port to listen
 		app.use(cors());
 		app.use(bodyParser.json());
 
@@ -38,7 +41,7 @@ createConnection()
 		app.post("/private", isAuth);
 
 		// start express server
-		app.listen(port, async () => {
+		app.listen(process.env.PORT || 3000, async () => {
 			// insert new users for test
 			// await connection.manager.save(
 			// 	connection.manager.create(User, {
@@ -62,3 +65,46 @@ createConnection()
 		// tslint:disable-next-line:no-console
 	})
 	.catch();
+
+
+const getOptions = async () =>  {
+    let connectionOptions: ConnectionOptions;
+    connectionOptions = {
+        type: 'postgres',
+        synchronize: false,
+        logging: false,
+		migrationsRun: true,
+		extra: {
+			ssl: {
+				rejectedUnautorized: false,
+			},
+		  },
+        entities: ['dist/entity/**/*.js'],
+	    migrations: ["dist/migration/**/*.js"],
+	    subscribers: ["dist/subscriber/**/*.js"],
+	    cli: {
+	        entitiesDir: "src/entity",
+		    migrationsDir: "src/migration",
+		    subscribersDir: "src/subscriber"
+	    }
+    };
+    if (process.env.DATABASE_URL) {
+        Object.assign(connectionOptions, { url: process.env.DATABASE_URL });
+      } else {
+        // gets your default configuration
+        // you could get a specific config by name getConnectionOptions('production')
+        // or getConnectionOptions(process.env.NODE_ENV)
+        connectionOptions = await getConnectionOptions();
+      }
+
+      return connectionOptions;
+};
+
+const connect2Database = async (): Promise<void> => {
+    const typeormconfig = await getOptions();
+    await createConnection(typeormconfig);
+};
+
+connect2Database().then(async () => {
+    console.log('Connected to database');
+});
