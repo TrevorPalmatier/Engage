47a104ba0d23a80c1ca7c8df3a77c84b557ac517
Kaley Wilson <kaley.wilson@hope.edu>
2022-02-08 16:33:46 -0500

    npm run build for study routes

--- server/dist/routes.js
+++ server/dist/routes.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const UserController_1 = require("./controller/UserController");
+const StudyController_1 = require("./controller/StudyController");
 exports.Routes = [{
         method: "get",
         route: "/users",
@@ -20,6 +21,26 @@ exports.Routes = [{
         method: "delete",
         route: "/users/:id",
         controller: UserController_1.UserController,
+        action: "remove"
+    }, {
+        method: "get",
+        route: "/studies",
+        controller: StudyController_1.StudyController,
+        action: "all"
+    }, {
+        method: "get",
+        route: "/studies/:id",
+        controller: StudyController_1.StudyController,
+        action: "one"
+    }, {
+        method: "post",
+        route: "/studies",
+        controller: StudyController_1.StudyController,
+        action: "save"
+    }, {
+        method: "delete",
+        route: "/studies/:id",
+        controller: StudyController_1.StudyController,
         action: "remove"
     }];
 //# sourceMappingURL=routes.js.map
