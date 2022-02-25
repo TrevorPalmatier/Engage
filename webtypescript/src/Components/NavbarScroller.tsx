565edf1f9f6ce6b65d395abcd238dc2ddb12972f
Kaley Wilson <kaley.wilson@hope.edu>
2022-02-24 23:07:32 -0500

    added nav bar

--- webtypescript/src/Components/NavbarScroller.tsx
+++ webtypescript/src/Components/NavbarScroller.tsx
@@ -1,27 +1,21 @@
 import * as React from 'react'
-import styled from 'styled-components';
+// import styled from 'styled-components';
+
+import "./navbarScroller.scss"
 
 const NavbarScroller = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="container-fluid">
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
+    // <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className="NavbarScroller">
 
-        <div className="collapse navbar-collapse" id="navbarColor02">
-          <ul className="navbar-nav me-auto">
-            <li className="nav-item">
-              <a className="nav-link active" href="/">Home</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link active" href="/viewstudies">Studies</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/About">About</a>
-            </li>
-          </ul>
-        </div>
+      <div className="left">
+          <p>ENGAGE</p>
+      </div>
+
+      <div className="right">
+        <li><a href="/">Home</a></li>
+        <li><a className="nav-link active" href="/viewstudies">Studies</a></li>
+        <li><a className="nav-link" href="/About">About</a></li>
       </div>
     </nav>
   )
