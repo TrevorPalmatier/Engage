6e1fc20ab3ac13d987b05b57b3ec622d697a4910
Kaley Wilson <kaley.wilson@hope.edu>
2022-03-29 18:03:36 -0400

    entry controller changes

--- server/src/controller/EntryController.ts
+++ server/src/controller/EntryController.ts
@@ -10,7 +10,7 @@ export class EntryController {
 	}
 
 	async one(request: Request, response: Response, next: NextFunction) {
-		return this.entryRepository.findOne(request.params.id);
+		return this.entryRepository.findOne(request.params.id, {relations: ["user"] });
 	}
 
 	async save(request: Request, response: Response, next: NextFunction) {
