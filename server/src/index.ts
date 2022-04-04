414e08b1276937c1d8bb213a3f1a1cce725be603
Trevor Palmatier <tspalmat5@gmail.com>
2022-04-03 22:19:06 -0400

    webapp authentication

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,7 +8,7 @@ import { Request, Response } from "express";
 import { Routes } from "./routes";
 import { createConnection } from "typeorm";
 import dotenv from "dotenv";
-import { validationResult } from "express-validator";
+// import { validationResult } from "express-validator";
 dotenv.config();
 
 function authCheck(req: Request, res: Response, next: NextFunction) {
@@ -36,19 +36,19 @@ createConnection()
 				origin: "*",
 			})
 		);
-		app.use(authCheck);
+		// app.use(authCheck);
 		app.use(bodyParser.json({ limit: "50mb" }));
 		app.use(bodyParser.urlencoded({ limit: "100mb", extended: true }));
 
 		// register express routes from defined application routes
 		Routes.forEach((route) => {
 			(app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
-					const result = new (route.controller as any)()[route.action](req, res, next);
-					if (result instanceof Promise) {
-						result.then((result) => (result !== null && result !== undefined ? res.send(result) : undefined));
-					} else if (result !== null && result !== undefined) {
-						res.json(result);
-					}
+				const result = new (route.controller as any)()[route.action](req, res, next);
+				if (result instanceof Promise) {
+					result.then((result) => (result !== null && result !== undefined ? res.send(result) : undefined));
+				} else if (result !== null && result !== undefined) {
+					res.json(result);
+				}
 			});
 		});
 
